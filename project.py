44b692e03686a955aed59da25a43c91240473702
ANANTA NARAYAN PARIDA <154269193+ananta-7@users.noreply.github.com>
2024-06-22 11:35:53 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,37 @@
+#snake,water gun game 
+import random
+
+computer=random.choice([-1,0,1])
+youstr=input("enter your choice")
+youDict={"s":1,"w":-1,"g":0}
+reverseDict={1:"snake",-1:"water",0:"gun"}
+
+you=youDict[youstr]
+
+print(f"you chose{reverseDict[you]}\ncomputer chose {reverseDict[computer]}")
+
+if(computer == you):
+    print("it is a draw")
+    
+else:
+    if(computer==-1 and you==1):
+        print("you win!")
+        
+        
+    elif(computer==-1 and you==0):
+        print("you loss!")
+
+    elif(computer==1 and you==-1):
+        print("you win!")
+        
+    elif(computer==1 and you==0):
+        print("you win!")    
+
+    elif(computer==0 and you==-1):
+        print("you win!")
+        
+    elif(computer==0 and you==1):
+        print("you win!")
+        
+    else:
+        print("something went wrong!")
